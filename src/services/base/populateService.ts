d70c8c64248293ede3a7be9d0dfa7801ace39edb
Tyler Zhang <tztylerzhang@gmail.com>
2017-11-02 23:33:22 -0400

    finished populateService base class

--- src/services/base/populateService.ts
+++ src/services/base/populateService.ts
@@ -1,24 +1,26 @@
-// import * as _ from 'lodash'
-// import Populater, { PopulatePair, PopulateOptions } from '../../utils/populater'
+import * as _ from 'lodash'
+import Populater, { PopulatePair, PopulateOptions, ResolveObjects } from '../../utils/populater'
 
-// export default abstract class PopulateService<Type, Key> {
-//   abstract async findNeededObjects (ids: Key[]): Promise<Type[]>
-//   abstract idToString (id: Key): string
+export default abstract class PopulateService<Type, Key> {
+  abstract async findNeededObjects (ids: Key[]): Promise<Type[]>
+  abstract objectToId (obj: Type): Key
+  abstract idToString (id: Key): string
+  abstract idEquals (id1: Key, id2: Key): boolean
 
-//   async populate (docs: Object | Object[], paths: PopulatePair[], options: Partial<PopulateOptions> = {}) {
-//     const populater = new Populater(docs, paths, options)
-//     const neededIds = _.uniqBy(populater.neededIds, (id: Key) => this.idToString(id))
+  async populate (docs: Object | Object[], paths: PopulatePair[], options: Partial<PopulateOptions> = {}) {
+    const populater = new Populater(docs, paths, options)
+    const neededIds = _.uniqBy(populater.neededIds, (id: Key) => this.idToString(id))
 
-//     const results: Type[] = await this.findNeededObjects(neededIds)
+    const results: Type[] = await this.findNeededObjects(neededIds)
 
-//     /**
-//      * @todo: Maybe optimize this by using a hashmap
-//      */
-//     // const resolveObjectArray: ResolveObjects[] = results.map(res => ({
-//     //   id: res.id,
-//     //   payload: res
-//     // }))
+    /**
+     * @todo: Maybe optimize this by using a hashmap
+     */
+    const resolveObjectArray: ResolveObjects[] = results.map(res => ({
+      id: this.idToString(this.objectToId(res)),
+      payload: res
+    }))
 
-//     // populater.resolve(resolveObjectArray, (a: ObjectID, b: ObjectID) => a.equals(b))
-//   }
-// }
+    populater.resolve(resolveObjectArray, this.idEquals)
+  }
+}
