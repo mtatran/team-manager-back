6634c3e20c8d854dee349545dd6148d2a57ef0e1
Tyler Zhang <tztylerzhang@gmail.com>
2017-11-03 00:20:13 -0400

    fix populater service

---
+++ docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Waterloop Team Manager API",
+  "url": "https://team-manager.teamwaterloop.ca/api",
+  "name": "team-manager-back",
+  "version": "1.0.0",
+  "description": "Backend for team manager website",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-11-03T04:19:59.127Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
