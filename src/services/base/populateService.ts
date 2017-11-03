b7d0315cf90436029986eb8b3a413e0c86426bfd
Tyler Zhang <tztylerzhang@gmail.com>
2017-11-02 23:42:43 -0400

    implement populate service for model service

--- src/services/base/populateService.ts
+++ src/services/base/populateService.ts
@@ -2,10 +2,6 @@ import * as _ from 'lodash'
 import Populater, { PopulatePair, PopulateOptions, ResolveObjects } from '../../utils/populater'
 
 export default abstract class PopulateService<Type, Key> {
-  abstract async findNeededObjects (ids: Key[]): Promise<Type[]>
-  abstract objectToId (obj: Type): Key
-  abstract idToString (id: Key): string
-  abstract idEquals (id1: Key, id2: Key): boolean
 
   async populate (docs: Object | Object[], paths: PopulatePair[], options: Partial<PopulateOptions> = {}) {
     const populater = new Populater(docs, paths, options)
@@ -23,4 +19,9 @@ export default abstract class PopulateService<Type, Key> {
 
     populater.resolve(resolveObjectArray, this.idEquals)
   }
+
+  protected abstract async findNeededObjects (ids: Key[]): Promise<Type[]>
+  protected abstract objectToId (obj: Type): Key
+  protected abstract idToString (id: Key): string
+  protected abstract idEquals (id1: Key, id2: Key): boolean
 }
